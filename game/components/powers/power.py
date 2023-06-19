5d30e4c5341022613f8d24d300dab6d1a5bd62e0
BrandonVillanuevaJalaUniversity <136256886+BrandonVillanuevaJalaUniversity@users.noreply.github.com>
2023-06-18 22:56:34 -0500

    parche items move

--- game/components/powers/power.py
+++ game/components/powers/power.py
@@ -1,5 +1,5 @@
 import pygame, random
-from game.utils.constants import SCREEN_WIDTH
+from game.utils.constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 class Power:
     POWER_WIDTH= 40
@@ -13,11 +13,73 @@ class Power:
         self.rect.x = random.randint(120, SCREEN_WIDTH-120) 
         self.type = type
         self.rect.y = 0
+        self.position = 'down_rigth'
         
     def update(self):
-        self.rect.y += self.POWER_SPEED
-    
+        if self.position == 'down_left':
+            self.down_left()
+        elif self.position == 'down_rigth':
+            self.down_rigth()
+        elif self.position == 'up_rigth':
+            self.up_rigth()
+        elif self.position == 'up_left':
+            self.up_left()
+        
+    def down_left(self):
+        if self.rect.x > 0 and self.rect.bottom < SCREEN_HEIGHT: 
+            self.rect.x -=10
+            self.rect.y += self.POWER_SPEED
+        elif self.rect.x > 0 and self.rect.bottom >= SCREEN_HEIGHT: 
+            self.position = 'up_left'
+        elif self.rect.x <= 0 and self.rect.bottom >= SCREEN_HEIGHT: 
+            self.position = 'up_rigth'            
+        elif self.rect.x < 0 and self.rect.bottom < SCREEN_HEIGHT: 
+            self.position = 'down_rigth'            
+            
+                    
+    def down_rigth(self):
+        if self.rect.x < SCREEN_WIDTH and self.rect.y < SCREEN_HEIGHT: 
+            self.rect.x +=10
+            self.rect.y += self.POWER_SPEED
+            
+        elif self.rect.x < SCREEN_WIDTH and self.rect.y >= SCREEN_HEIGHT: 
+            self.position = 'up_rigth'
+            
+        elif self.rect.x >= SCREEN_WIDTH and self.rect.y < SCREEN_HEIGHT: 
+            self.position = 'down_left'  
+            
+        elif self.rect.x >= SCREEN_WIDTH and self.rect.y >= SCREEN_HEIGHT: 
+            self.position = 'up_left'  
+            
+            
+    def up_left(self):
+        if self.rect.x > 0 and self.rect.y > 0: 
+            self.rect.x -=10
+            self.rect.y -= self.POWER_SPEED
+            
+        elif self.rect.x <= 0 and self.rect.y > 0: 
+            self.position = 'up_rigth'
+            
+        elif self.rect.x > 0 and self.rect.top <= 0: 
+            self.position = 'down_left'
+            
+        elif self.rect.x <= 0 and self.rect.y <= 0: 
+            self.position = 'down_rigth'                        
+        
+    def up_rigth(self):
+        if self.rect.x < SCREEN_WIDTH and self.rect.y > 0: 
+            self.rect.x +=10
+            self.rect.y -= self.POWER_SPEED
+            
+        elif self.rect.x >= SCREEN_WIDTH and self.rect.y > 0: 
+            self.position = 'up_left'
+            
+        elif self.rect.x >= SCREEN_WIDTH and self.rect.y <= 0: 
+            self.position = 'down_left'
+            
+        elif self.rect.x < SCREEN_WIDTH and self.rect.y <= 0: 
+            self.position = 'down_rigth'   
+        
+        
     def draw(self,screen):
         screen.blit(self.image, self.rect)
-
-        
